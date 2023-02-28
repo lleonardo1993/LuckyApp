24e11727f3a4177ba522b342ff5d5a04ba9a96a7
Leonardo <lleonardo1555@gmail.com>
2023-02-28 14:01:55 -0300

    configuracao Context e, string connection

---
+++ LuckyApp/Config/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using LuckyApp.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuckyApp.Config
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+            Database.EnsureCreated(); // se não existir db cria uma db
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+    }
+}
---
+++ LuckyApp/Model/Produto.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading.Tasks;
+
+namespace LuckyApp.Model
+{
+    [Table("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Descricao")]
+        public string Descricao { get; set; }
+    }
+}
